373a8606cd5328514b6cf3bbab80f0ad021bdbac
Istiak Ahmed <mimalahmed6219@gmail.com>
2024-05-05 22:07:45 +0600

    setup auth controller route

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,17 @@
+export const signup = (req, res) => {
+  res.json({
+    message: "signup routes",
+  });
+};
+
+export const login = (req, res) => {
+  res.json({
+    message: "Login routes",
+  });
+};
+
+export const logout = (req, res) => {
+  res.json({
+    message: "Logout routes",
+  });
+};

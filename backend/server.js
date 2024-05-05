c0b55e3853773a5e81a1792f579da0fa3d164ca7
Istiak Ahmed <mimalahmed6219@gmail.com>
2024-05-05 22:07:18 +0600

    add route

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
-import { config } from "dotenv";
+import dotenv from "dotenv";
 import express from "express";
-config();
+
+import authRoutes from "./routes/auth.routes.js";
+
+dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT || 5000;
@@ -10,6 +13,10 @@ app.get("/", (req, res) => {
     message: "Hello world",
   });
 });
+
+app.use("/api/auth", authRoutes);
+// app.use("/api/messages", messageRoutes);
+// app.use("/api/users", userRoutes);
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
 });

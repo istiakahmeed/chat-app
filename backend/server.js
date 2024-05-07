5ad677ecf4f2a328dd7713fd6300b1d106c23740
Istiak Ahmed <mimalahmed6219@gmail.com>
2024-05-07 21:20:02 +0600

    add signup route

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,25 @@
 import dotenv from "dotenv";
 import express from "express";
 
+import connectToMongoDB from "../db/connectToMongoDB.js";
 import authRoutes from "./routes/auth.routes.js";
 
-dotenv.config();
-const app = express();
-
 const PORT = process.env.PORT || 5000;
+const app = express();
+dotenv.config();
 
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+// app.use("/api/messages", messageRoutes);
+// app.use("/api/users", userRoutes);
 app.get("/", (req, res) => {
   res.json({
     message: "Hello world",
   });
 });
 
-app.use("/api/auth", authRoutes);
-// app.use("/api/messages", messageRoutes);
-// app.use("/api/users", userRoutes);
 app.listen(PORT, () => {
+  connectToMongoDB();
   console.log(`server is running on port ${PORT}`);
 });

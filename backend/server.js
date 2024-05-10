2d3234465bb3ce0a141d09831977a8d8ffff4705
Istiak Ahmed <mimalahmed6219@gmail.com>
2024-05-10 15:51:52 +0600

    complete everything in backend

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,21 @@
 import dotenv from "dotenv";
 import express from "express";
-
+import cookieParser from "cookie-parser";
 import connectToMongoDB from "../db/connectToMongoDB.js";
 import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
 
 const PORT = process.env.PORT || 5000;
 const app = express();
 dotenv.config();
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
-// app.use("/api/messages", messageRoutes);
-// app.use("/api/users", userRoutes);
-app.get("/", (req, res) => {
-  res.json({
-    message: "Hello world",
-  });
-});
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => {
   connectToMongoDB();

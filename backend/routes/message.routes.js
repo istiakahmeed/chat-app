afcc4afb6b25434194d0ab258760b03b3c82a5f2
Istiak Ahmed <mimalahmed6219@gmail.com>
2024-05-10 15:41:22 +0600

    add sendmessage functionality

--- backend/routes/message.routes.js
+++ backend/routes/message.routes.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { getMessages } from "../controllers/message.controller.js";
+import protectRoute from "../middleware/protectRoute.js";
 
 const router = express.Router();
 
-router.get("/:id", getMessages);
+router.get("/:id", protectRoute, getMessages);
 router.post("/send/:id", protectRoute, sendMessage);
 
 export default router;

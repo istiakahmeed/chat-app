d6a9731854485c940103da0b0fd2f5d3dddac7e1
Istiak Ahmed <mimalahmed6219@gmail.com>
2024-05-17 20:13:43 +0600

    some issue solve

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -4,7 +4,7 @@ import generateTokenAndSetCookie from "../utils/generateToken.js";
 
 export const signup = async (req, res) => {
   try {
-    const { fullName, username, password, confirmPassword } = req.body;
+    const { fullName, username, password, confirmPassword, gender } = req.body;
 
     if (password !== confirmPassword) {
       return res.status(400).json({ message: "Passwords do not match" });

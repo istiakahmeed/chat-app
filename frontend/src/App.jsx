ed82b038269040f21894ccad9fd30c3b4bf65505
Istiak Ahmed <mimalahmed6219@gmail.com>
2024-05-17 20:15:13 +0600

    add router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,17 @@
+import { Toaster } from "react-hot-toast";
+import { Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
-
+import Login from "./pages/login/Login";
+import SignUp from "./pages/signup/SignUp";
 function App() {
   return (
     <div className="p-4 h-screen flex items-center justify-center">
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+      <Toaster />
     </div>
   );
 }
--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 /* eslint-disable react/no-unescaped-entities */
 const Login = () => {
   return (
@@ -29,12 +31,12 @@ const Login = () => {
               className="w-full input input-bordered h-10"
             />
           </div>
-          <a
-            href="#"
+          <Link
+            to="/signup"
             className="text-sm hover:underline hover:text-blue-600 mt-2 inline-block"
           >
             don't have an account
-          </a>
+          </Link>
           <div>
             <button className="btn btn-block btn-sm mt-2">Login</button>
           </div>

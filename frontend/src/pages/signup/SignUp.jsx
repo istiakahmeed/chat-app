7d81d84bc9da6b53f730033f58a000c915eb642a
Istiak Ahmed <mimalahmed6219@gmail.com>
2024-05-19 05:47:27 +0600

    add loading state

--- frontend/src/pages/signup/SignUp.jsx
+++ frontend/src/pages/signup/SignUp.jsx
@@ -13,7 +13,7 @@ const SignUp = () => {
     gender: "",
   });
 
-  const { signup } = useSignup();
+  const { loading, signup } = useSignup();
 
   const handleCheckboxChange = (gender) => {
     setInputs({ ...inputs, gender });
@@ -100,8 +100,15 @@ const SignUp = () => {
             Already have an account
           </Link>
           <div>
-            <button className="btn btn-block btn-sm mt-2 border border-slate-700">
-              Sign Up
+            <button
+              className="btn btn-block btn-sm mt-2 border border-slate-700"
+              disabled={loading}
+            >
+              {loading ? (
+                <span className="loading loading-spinner"></span>
+              ) : (
+                "Sign Up"
+              )}
             </button>
           </div>
         </form>

20b64f384621e9721513e1788b7e59914103414a
Istiak Ahmed <mimalahmed6219@gmail.com>
2024-05-20 21:12:52 +0600

    add random emojis

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,6 +1,9 @@
+import User from "../models/user.model.js";
+
 export const getUsersForSidebar = async (req, res) => {
   try {
     const loggedInUserId = req.user._id;
+
     const filteredUsers = await User.find({
       _id: { $ne: loggedInUserId },
     }).select("-password");
